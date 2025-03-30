17733679df552dced871d3de2cd3418fa1305f21
sauravdeyshuvo <sauravshuvo@yahoo.com>
2025-03-30 01:34:17 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,26 @@
 from setuptools import setup, find_packages
 
+# Read the contents of your README.md file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="asos_analysis",  # Name of your library
-    version="1.0.0",  # Initial version of your library
+    version="1.0.0",  # Initial version
     description="A Python tool for sorting and analyzing ASOS weather data",  # Brief description
+    long_description=long_description,  # Detailed description (from README.md)
+    long_description_content_type="text/markdown",  # Specify that the long description is in Markdown
     author="Saurav",  # Your name
-    author_email="your_email@example.com",  # Your contact email
-    url="https://github.com/your_github_username/asos_analysis",  # URL to your GitHub repo or website
-    packages=find_packages(),  # Automatically find all packages in your library folder
+    author_email="your_email@example.com",  # Your email
+    url="https://github.com/sauravshuvo/asos_analysis",  # GitHub repository URL
+    packages=find_packages(),  # Automatically detect packages
     install_requires=[
         "pandas>=1.3.0",
         "openpyxl>=3.0.9",
-    ],  # Dependencies your library needs
+    ],  # Dependencies
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",  # License type
+        "License :: OSI Approved :: MIT License",  # License
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",  # Minimum Python version
